a70126ef02fdf3a3f5f04f39305bf49f998880a0
Lautaro Salas Moyano <lautito_96@hotmail.com>
2020-11-04 22:30:26 -0300

    aaa

--- src/main/java/Interfaz/Controller.java
+++ src/main/java/Interfaz/Controller.java
@@ -1,6 +1,8 @@
 package Interfaz;
+import Soporte.TextFile;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.DirectoryChooser;
 import java.io.File;
 
@@ -14,6 +16,17 @@ public class Controller {
         archivo = dc.showDialog(null);
         if (archivo != null)
             lblOrigen.setText(archivo.getPath());
+
+    }
+
+    public void cargarDatos(ActionEvent actionEvent) {
+
+        TextFile archAgrupaciones = new TextFile(lblOrigen.getText()+ "\\descripcion_postulaciones.dsv");
+        archAgrupaciones.identificarAgrupaciones();
+        TextFile archRegiones = new TextFile(lblOrigen.getText() + "\\descripcion_regiones.dsv");
+        TextFile archMesas = new TextFile(lblOrigen.getText() + "\\mesas_totales_agrp_politica.dsv");
+
+
 
     }
 }
